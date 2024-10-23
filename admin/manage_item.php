86c07b457ed0a6a7ef00472737a64fd15b15da76
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2024-10-23 23:00:21 +0700

    test

---
+++ admin/manage_item.php
@@ -0,0 +1,9 @@
+<?php include('partials/header.php'); ?>
+
+<!-- Body -->
+<div class="container mt-5">
+    <h2>Manage Item</h2>
+
+</div>
+
+<?php include('partials/footer.php'); ?>
---
+++ admin/partials/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap E-Commerce Template</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
+        rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-4 fs-6">
+                        <a class="nav-link active" href="index_admin.php">หน้าแรก</a>
+                    </li>
+                    <li class="nav-item me-4 fs-6">
+                        <a class="nav-link" href="manage_admin.php">ผู้ดูแล</a>
+                    </li>
+                    <li class="nav-item me-4 fs-6">
+                        <a class="nav-link" href="manage_item.php">จัดการสินค้า</a>
+                    </li>
+                    <li class="nav-item me-4 fs-6">
+                        <a class="nav-link" href="manage_order.php">รายการคำสั่งซื้อ</a>
+                    </li>
+                    <li class="nav-item me-4 fs-6">
+                        <a class="nav-link" href="#">ออกจากระบบ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
