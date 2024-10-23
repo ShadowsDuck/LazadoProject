925da9e1ab5f93b4104dd122e50ec671dbccbdaa
fahaph <F_Phonwen@hotmail.com>
2024-10-24 01:23:57 +0700

    sql add admin account

--- admin/manage_admin.php
+++ admin/manage_admin.php
@@ -8,7 +8,8 @@
     <!-- <a href="add_admin.php" class="btn btn-primary mt-2 mb-4"> Add Admin </a> -->
 
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary mt-2 mb-4 float-end" data-bs-toggle="modal" data-bs-target="#addAdminModal">
+    <button type="button" class="btn btn-primary mt-2 mb-4 float-end" data-bs-toggle="modal"
+        data-bs-target="#addAdminModal">
         Add Admin
     </button>
 
@@ -16,28 +17,33 @@
     <div class="modal fade" id="addAdminModal" tabindex="-1" aria-labelledby="addAdminModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addAdminModalLabel">Add Admin</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="form-group mb-3">
-                        <label for="">Fullname</label>
-                        <input type="text" class="form-control" placeholder="Enter Fullname">
+                <form action="add_admin.php" method="post">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addAdminModalLabel">Add Admin</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="form-group mb-3">
-                        <label for="">Username</label>
-                        <input type="text" class="form-control" placeholder="Enter Username">
+
+                    <div class="modal-body">
+                        <div class="form-group mb-3">
+                            <label for="">Fullname</label>
+                            <input type="text" class="form-control" placeholder="Enter Fullname" name="fullname">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Username</label>
+                            <input type="text" class="form-control" placeholder="Enter Username" name="username">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Password</label>
+                            <input type="text" class="form-control" placeholder="Enter Password" name="password">
+                        </div>
                     </div>
-                    <div class="form-group mb-3">
-                        <label for="">Password</label>
-                        <input type="text" class="form-control" placeholder="Enter Password">
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add</button>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Add</button>
-                </div>
+                </form>
+
             </div>
         </div>
     </div>
