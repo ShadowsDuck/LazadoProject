e596e16a2118d7c8da96cbd40ec6947dca867417
ratchanona65 <ratchanona65@nu.ac.th>
2024-10-26 02:24:32 +0700

    #addeditpage

---
+++ user/editpage/infoEdit.php
@@ -0,0 +1,34 @@
+<div class="container ms-1" style="border-radius: 5px;">
+    <div class="d-flex justify-content-between align-items-center mb-4">
+        <h4>ข้อมูลส่วนตัว</h4>
+        <button class="btn btn-edit">แก้ไขข้อมูลส่วนตัว</button>
+    </div>
+
+    <div class="user-info d-flex align-items-center">
+        <div>
+            <h5>มาร์ค คคค</h5>
+            <p>lhwza007x2@gmail.com</p>
+        </div>
+    </div>
+
+    <div class="row text-center my-4">
+        <div class="col-md-4">
+            <div class="status-card">
+                <p>0</p>
+                <span>เสร็จสิ้น</span>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="status-card">
+                <p>0</p>
+                <span>จัดส่งแล้ว</span>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="status-card">
+                <p>0</p>
+                <span>รอดำเนินการ</span>
+            </div>
+        </div>
+    </div>
+</div>
