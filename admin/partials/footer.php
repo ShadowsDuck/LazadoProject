d5863729e4e3ea937761129454f128a287f0f2f9
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2024-10-27 04:40:34 +0700

    remove footer in admin

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,22 +1,3 @@
-<!-- Footer -->
-<footer class="bg-dark text-white py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3">
-                <h5>Exclusive</h5>
-                <p>Get 10% off your first order</p>
-            </div>
-            <div class="col-md-3">
-                <h5>Support</h5>
-                <ul class="list-unstyled">
-                    <li><a href="#" class="text-white">FAQ</a></li>
-                </ul>
-            </div>
-            <!-- เพิ่มเนื้อหาส่วนท้ายเพิ่มเติมได้ที่นี่ -->
-        </div>
-    </div>
-</footer>
-
 <!-- JavaScript -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
