9f27c3ac0d685a8bb57b4824b98c6f71ec0005bf
fahaph <F_Phonwen@hotmail.com>
2024-10-27 01:57:21 +0700

    #

--- user/del_cart.php
+++ user/del_cart.php
@@ -6,7 +6,7 @@ if (isset($_GET["id"])) {
     $cart_id = $_GET['id'];
     $sql = "DELETE FROM cart WHERE id = '$cart_id'";
     $result = mysqli_query($conn, $sql);
-    header("Location:{$base_url}/user/kart.php");
+    header("Location:{$base_url}/user/cart.php");
 }else{
     die(header("Location:{$base_url}/login/error.php"));
 }
