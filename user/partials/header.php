4ffd71adf6bed3f1b4c907e015465096c50b78ef
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2024-10-27 06:11:03 +0700

    change font

--- user/partials/header.php
+++ user/partials/header.php
@@ -32,6 +32,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai&display=swap" rel="stylesheet">
 </head>
 
 <body>
