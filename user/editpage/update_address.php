bdb151c5eeb353f95eb2ba76023f6dea0ec0a58f
ratchanona65 <ratchanona65@nu.ac.th>
2024-10-26 08:20:01 +0700

    #

--- user/editpage/update_address.php
+++ user/editpage/update_address.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // อัพเดตข้อมูลในฐานข้อมูล
     if (mysqli_query($conn, $update_sql)) {
-        header("Location:{$base_url}/user/user_edit.php?page=addressEdit");
+        header("Location:{$base_url}/user/user_edit.php?page=infoEdit");
         exit; 
     } else {
         echo "Error updating record: " . mysqli_error($conn);
