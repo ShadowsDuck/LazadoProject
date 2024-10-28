95993460afde9df6c54f1c156b4b6310a432f200
ratchanona65 <ratchanona65@nu.ac.th>
2024-10-28 21:46:24 +0700

    #

--- user/partials/footer.php
+++ user/partials/footer.php
@@ -96,7 +96,9 @@
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
 
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
 
 
 
