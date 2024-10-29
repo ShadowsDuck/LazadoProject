842bfe51bd310c324b5f3d26e0b7d1e06388298f
fahaph <F_Phonwen@hotmail.com>
2024-10-29 16:57:44 +0700

    #

--- login/signup.php
+++ login/signup.php
@@ -33,16 +33,6 @@ include '../connect.php';
 
 <body>
     <div class="container min-vh-100 min-vw-100 d-flex justify-content-center align-items-center">
-        <div class="alert-container">
-            <?php if (!empty($_SESSION['message'])): ?>
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <?php echo $_SESSION['message']; ?>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-                <?php unset($_SESSION['message']); ?>
-            <?php endif; ?>
-        </div>
-
         <div class="row w-100">
             <!-- Left side: Image -->
             <div class="col-md-7 d-none d-md-flex align-items-center">
